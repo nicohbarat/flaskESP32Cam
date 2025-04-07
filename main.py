aa0b4e51974595a2076b68c6530eed5dce72c6c2
nicodemus <nico.hbarat@gmail.com>
2025-04-07 19:30:19 +0700

    comm lagi

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 # coding: utf-8
-from flask import Flask
+from flask import Flask,jsonify,request
 app = Flask(__name__)
 
 @app.route('/')
 def entry_point():
-	return 'Hellow, World!'
+	return jsonify(message = 'Selamat datang di Root!')
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(host='0.0.0.0', port=5000,debug=True)
 	
